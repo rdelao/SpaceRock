f509a881e56c64d3a2e59df7246562b20533caf7
Shea Polansky <env.laser@gmail.com>
2017-02-09 10:05:28 -0700

    Moved classes to correct package

--- src/SpaceRockGUI.java
+++ src/SpaceRockGUI.java
@@ -1,9 +1,3 @@
-package SpaceRockGUI;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
