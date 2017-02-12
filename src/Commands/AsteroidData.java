bca4f2e01005c3bbc4344f5348482587ea888f6b
David <davidmringo@gmail.com>
2017-02-12 16:03:57 -0700

    Add AsteroidData interface
    Hopefully we can use this interface (or something like it) to make plugging codebases together a little less painless, assuming the satellite data object can implement this interface.

---
+++ src/Commands/AsteroidData.java
@@ -0,0 +1,28 @@
+package Commands;
+
+import java.awt.geom.Point2D;
+import java.io.Serializable;
+
+/**
+ * AsteroidData is an interface for accessing the minimal data required
+ * by the operator software to model objects in the the view of
+ * SpaceRock camera.
+ */
+public interface AsteroidData extends Serializable {
+
+    /**
+     * @return the {@code Point2D} location (likely, centerpoint) of this
+     * asteroid within the view of the SpaceRock system.
+     */
+    Point2D getLoc();
+
+    /**
+     * @return the size (likely, diameter) of this asteroid
+     */
+    double getSize();
+
+    /**
+     * @return the Satellite-assigned ID of this asteroid
+     */
+    long getID();
+}
