a35c3ed373b0bc085059711ed3a16e98c03a2660
David <davidmringo@gmail.com>
2017-02-14 12:10:33 -0700

    Asteroid implements AsteroidData. Remove some fields.
    Add small doc comment. Removed `heading` and `rawImage` members as well.  The `rawImage` should only be sent upon request. `heading` will likely be determined during client-side analysis (based on comments in class).

--- src/Commands/Asteroid.java
+++ src/Commands/Asteroid.java
@@ -1,25 +1,53 @@
 package Commands;
 
-import java.awt.*;
 import java.awt.geom.Point2D;
-import java.io.Serializable;
 
 /**
- * Created by magik on 2/5/2017.
+ * Created by magik on 2/5/2017. Modified by David R., 2/12/2017
+ * <p>
+ * The Asteroid class describes an immutable data object that _should_
+ * be roughly isomorphic to what will be sent by the SpaceRock satellite code.
+ * It implements {@code AsteroidData} which should help interface between
+ * disparate code bases.
  */
-public class  Asteroid implements Serializable{
-    public Point2D location;
-    public Point2D heading;
-    public int id;
-    public double size;
-    public Image rawImage;
+public class Asteroid implements AsteroidData {
+
+    public final Point2D location;
+    public final long id;
+    public final double size;
 
 
-    public Asteroid(Point2D location,Point2D heading,int id,double size, Image rawImage){
-        this.location=location;
-        this.heading=heading;
-        this.id=id;
-        this.size=size;
-        this.rawImage = rawImage;
+    public Asteroid(Point2D location, long id, double size) {
+        this.location = location;
+        this.id = id;
+        this.size = size;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Asteroid{" +
+               "location=" + location +
+               ", id=" + id +
+               ", size=" + size +
+               '}';
+    }
+
+
+    @Override
+    public Point2D getLoc() {
+        return location;
+    }
+
+
+    @Override
+    public double getSize() {
+        return size;
+    }
+
+
+    @Override
+    public long getID() {
+        return id;
     }
 }
