64357a2d9cb65fa85693d8bcb43ad0d434888c41
brendan-donohoe <blackmoondin@gmail.com>
2017-02-16 14:13:41 -0700

    Commented DebrisProcessor a bit more.
    Commented DebrisProcessor a bit more and corrected the center-grabbing.

--- src/Processing/DebrisProcessor.java
+++ src/Processing/DebrisProcessor.java
@@ -1,5 +1,5 @@
 /**
- WARNING: NOT YET TESTED.
+ WARNING: NOT YET TESTED - MAY STILL NEED SOME SUBSTANTIAL WORK.
  */
 
 package Processing;
@@ -16,15 +16,23 @@ import java.util.HashSet;
  *
  * Relevant assumptions:
  *
- * The debris are roughly spherical in shape.
- * The debris have approximately constant velocity.
- * The "location" field in the Asteroid class denotes the upper-left coordinate
- * of the asteroid, in screen coordinates.
+ * The debris are approximable as spheres in shape.
+ * The debris have approximately linear trajectories.
+ * The "location" field in the Asteroid class denotes the center of the
+ * asteroid, in screen coordinates.
  * The "size" field denotes the diameter of the asteroid as detected by the
  * camera, also in screen coordinates.
  */
 public class DebrisProcessor {
     public static final int NUM_CONFIGURATIONS = 3;
+ 
+    /**
+     Currently an arbitrary constant value - may have to be tweaked in
+     accordance with the sensitivity of the chosen coordinate system.
+     If the computed cross product of vectors from the centers of three
+     asteroids is less than this value, they pass the cross product test for
+     being along a straight line.
+     */
     public static final double CROSS_ERROR = 0.1;
 
     private Asteroid[][] configurations;
@@ -50,6 +58,10 @@ public class DebrisProcessor {
      @return true if labels are assigned to the asteroids, false otherwise.
      */
     public boolean addAndAssign(Asteroid[] configuration) {
+     
+        // Add the newest configuration to the working set and evict the
+        // least recent.
+     
         if (curIdx < NUM_CONFIGURATIONS - 1) {
             configurations[curIdx++] = configuration;
             return false;
@@ -81,8 +93,9 @@ public class DebrisProcessor {
      where a1 is least recent and a3 is the most recent, they are considered to
      be the same asteroid and given the same label if:
      <p>
-     The centers of a1, a2, and a3 are situated on a straight line relative to
-     the viewing plane.
+     The centers of a1, a2, and a3 trace out a straight line relative to the
+     viewing plane (this should be the case if the trajectories are linear, even
+     in three dimensions).
      <p>
      a1, a2, and a3 are displaced progressively leftward or rightward from each
      other (in other words, the asteroid moves solely to the left or to the
@@ -93,6 +106,14 @@ public class DebrisProcessor {
      other.
      <p>
      a1, a2, and a3 grow progressively larger or smaller.
+     <p>
+     Note that this only assigns labels to those asteroids deemed to be the
+     same.  It's possible that some asteroids that aren't added to a triplet
+     will be left unassigned.  This may just mean that we haven't yet collected
+     enough images of that particular one to assign an id quite yet.  (It's
+     also possible that an asteroid that ends up hidden by another overlapping
+     asteroid will end up having to be reassigned by the processor.  This is
+     unfortunate, but ideally shouldn't be a huge issue.)
      */
     public void assignLabels() {
         HashSet<Long> usedIndices = new HashSet<>();
@@ -102,22 +123,26 @@ public class DebrisProcessor {
         for (int i = 0; i < configurations[0].length; i++) {
             Asteroid a1 = configurations[0][i];
 
-            double x1 = a1.location.getX() + a1.size / 2;
-            double y1 = a1.location.getY() + a1.size / 2;
+            // We get the centerpoint of each asteroid.  Note that I'm
+            // assuming here that getX() and getY() return the center
+            // of each asteroid by default.
+         
+            double x1 = a1.location.getX();
+            double y1 = a1.location.getY();
 
             for (int j = 0; j < configurations[1].length; j++) {
                 Asteroid a2 = configurations[1][j];
 
-                double x2 = a2.location.getX() + a2.size / 2;
-                double y2 = a2.location.getY() + a2.size / 2;
+                double x2 = a2.location.getX();
+                double y2 = a2.location.getY();
 
                 Vector2 v1 = new Vector2(x2 - x1, y2 - y1);
 
                 for (int k = 0; k < configurations[2].length; k++) {
                     Asteroid a3 = configurations[2][k];
 
-                    double x3 = a3.location.getX() + a3.size / 2;
-                    double y3 = a3.location.getY() + a3.size / 2;
+                    double x3 = a3.location.getX();
+                    double y3 = a3.location.getY();
 
                     Vector2 v2 = new Vector2(x3 - x2, y3 - y2);
 
@@ -141,21 +166,28 @@ public class DebrisProcessor {
                     if (errCross < CROSS_ERROR && (rightward || leftward) &&
                         (upward || downward) && (forward || backward) &&
                         !usedIndices.contains(a2.id)) {
-                        // These asteroids are considered the same.  We will now assign it
-                        // an id.
+                        // This triplet is considered the same.  We will now assign it an
+                        // id.
+                     
+                        // TODO: Note that these criteria are very strict.  If an asteroid
+                        // is measured to move even a little bit in an unexpected direction,
+                        // the entire thing will fail.  It might be best to do something
+                        // similar to the cross product above, and permit a certain amount
+                        // of "error" into the calculation before rejecting it as opposed to
+                        // using the unforgiving booleans above.
 
                         if (a2.id == 0) {
                             // This is the first time we've begun to track this asteroid (we
-                            // haven't given it an id previously) - assign it its own unique
+                            // haven't given it an id previously) - assign it a unique
                             // label.
 
                             a1.id = currentId;
                             a2.id = currentId;
                             a3.id = currentId++;
                         } else {
-                            // We've already been tracking this asteroid - simply assign the
-                            // most recent version of this asteroid the id of its
-                            // predecessor.
+                            // We've already been tracking this particular asteroid - this
+                            // is just the next step in its motion.  So, just give it the
+                            // id of its predecessor.
 
                             a3.id = a2.id;
                         }
@@ -170,7 +202,29 @@ public class DebrisProcessor {
         }
     }
 
-
+    /**
+     TODO: Assign threat level.  Should hopefully not be terrible to do.  My idea was to
+     measure the average amount that the size changes with respect to time for each
+     growing asteroid.  The timestamps will be needed to get the average increase in
+     radius, but as objects closer to us appear to "grow" faster, you can get a good
+     idea of how close you are to colliding with the asteroid most likely by comparing
+     the rates of growth of each asteroid.  If they're increasing, and increasing by a
+     significant margin, collision is probably going to be pretty imminent, and that'd
+     correspond to a more "dangerous" threat level.
+    */
+ 
+    /**
+     TODO: Assign velocity.  I'm not sure there's a good way to do it.  A two-dimensional
+     image of a three-dimensional view with no distances known makes it pretty much
+     impossible to assign exact values as there's so much ambiguity.  I.e. if you see a
+     tiny rock on your screen flit past your screen quickly, that could be either a
+     dime-sized rock moving a couple of inches away from the lens (in which case, the
+     velocity of that particular asteroid is low) or a rock the size of the Sun moving
+     several million kilometers away (in which case, the velocity of that particular
+     asteroid would be ridiculously high) due to the fact that the camera can't emulate
+     depth perception.  Maybe there's some way it can be estimated.
+    */
+ 
     /**
      Utility vector class for use in assigning labels to the asteroids.
      */
