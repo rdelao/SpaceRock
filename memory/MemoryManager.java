47277e6ec7654d48598e179d7c7030757c82c172
erparks <erparks24@gmail.com>
2017-02-28 00:38:47 -0700

    Started interface

--- memory/MemoryManager.java
+++ memory/MemoryManager.java
@@ -1,9 +1,14 @@
-package memory;
-
 /**
  * Created by Ethan on 2/28/2017.
  */
 public interface MemoryManager {
+
+    //Methods to be called by the FPGA
+    boolean getRawImageRequested();
+
+    //Methods to be called by the PC
+    void setRawImageRequested();
+
 
 
 }
