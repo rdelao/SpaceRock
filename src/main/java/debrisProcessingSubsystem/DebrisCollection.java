2fbc17607b0fb72ccc23a1d901b56d3a00bea0d0
justindonthomas <justin.don.thomas@gmail.com>
2017-03-04 07:45:56 -0700

    Created debrisProcessingSubsystem, placeholder debris collection interface.

---
+++ src/main/java/debrisProcessingSubsystem/DebrisCollection.java
@@ -0,0 +1,25 @@
+package debrisProcessingSubsystem;
+
+/**
+ * This will be the interface implemented by the Debris Collection object shown
+ * in the SADD.
+ * This is a preliminary placeholder and very subject to change.
+ * Created by jdt on 3/4/17.
+ */
+public interface DebrisCollection {
+    //TODO Uncomment when there types are created.
+    /**
+     * This method will be called when it is time to add a new image to the
+     * debris list.
+     */
+    //public boolean newImage();
+
+    /**
+     * This method is to be called when. SADD team appears to envision this
+     * implemented with an iterator.
+     * possibly within the debris list itself?
+     */
+    //public Debris getDebris();
+
+    //methods swapList and clearList should be internal per prof roman.
+}
